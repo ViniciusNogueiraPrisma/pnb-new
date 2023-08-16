002925a30aa4a3cd746c9829a0c7d1a193ab32f7
Vinicius Nogueira <v.francisco@riprisma.com>
2023-08-16 14:58:00 -0300

    accordion suave

--- js/main.js
+++ js/main.js
@@ -652,6 +652,43 @@ if (window.matchMedia("(min-width: 991px)").matches) {
 //   $(".owl-carousel").trigger("refresh.owl.carousel");
 // });
 
+const accordion_item = document.querySelectorAll(".accordion_item");
+
+accordion_item.forEach((item) => {
+  const accordion_header_item = item.querySelector(".accordion_header");
+
+  accordion_header_item.addEventListener("click", () => {
+    const accordion_content_item = item.querySelector(".accordion_content");
+
+    const item_actived = document.querySelector(".active");
+
+    VerifyActive(item, accordion_content_item, item_actived);
+  });
+});
+
+function VerifyActive(item, content, content_actived) {
+  const icon_item = item.querySelector(".icon");
+  const icon_item_active = document.querySelectorAll(".icon");
+
+  icon_item_active.forEach(
+    (item) =>
+      (item.innerHTML =
+        "<img src='./images/new-images/icons/icon-+.svg' alt='' />")
+  );
+
+  if (content_actived) {
+    content_actived.style.height = 0;
+    content_actived.classList.remove("active");
+  }
+
+  if (content !== content_actived) {
+    icon_item.innerHTML =
+      "<img src='./images/new-images/icons/icon-menos-.svg' alt=''>";
+    content.classList.add("active");
+    content.style.height = content.scrollHeight + 10 + "px";
+  }
+}
+
 const produto1 = document.getElementById("produto1");
 const produto2 = document.getElementById("produto2");
 const produto3 = document.getElementById("produto3");
