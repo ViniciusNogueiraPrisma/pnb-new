39b37b3a40dbc02330bf12bc2f2cb53eeac6370c
Vinicius Nogueira <v.francisco@riprisma.com>
2023-08-21 14:44:23 -0300

    alt

--- js/carrosel.js
+++ js/carrosel.js
@@ -1,10 +1,10 @@
 let swiperHome = new Swiper(".mySwiper", {
   slidesPerView: 2,
   spaceBetween: 30,
-  // autoplay: {
-  //   delay: 2000,
-  //   disableOnInteraction: true,
-  // },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: true,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
