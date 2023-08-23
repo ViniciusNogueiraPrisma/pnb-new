3ac48aa205a099ceaab8f340a5f49a76084d2e03
vinicius nogueira <v.francisco@riprisma.com>
2023-08-22 21:07:37 -0300

    ultimas alterações css

--- vendor/menuFixo.js
+++ vendor/menuFixo.js
@@ -1,38 +1,57 @@
-function debounce(callback, delay) {
-  let timer;
-  return (...args) => {
-    if (timer) clearTimeout(timer);
-    timer = setTimeout(() => {
-      callback(...args);
-      timer = null;
-    }, delay);
-  };
-}
+let offset = 0;
+let isMenuActive = false; // Variável para verificar se o menu está ativo
 
-let offset = 0
-function menuFixo(){
-  const header = document.querySelector('.headerFixoMenu')
-  if(header){
-    if(window.pageYOffset > header.clientHeight && window.pageYOffset < 450 ){
-      header.classList.add('menu-bar')
-      setTimeout(()=> header.style.top = '0', 100)
-    }else if(window.pageYOffset>=header.clientHeight && header.classList.contains('menu-bar') && offset <= window.pageYOffset){
-      header.style.top = ''
-      setTimeout(()=> header.classList.remove('menu-bar'), 600) 
-    } else if(offset > window.pageYOffset && !header.classList.contains('menu-bar') ){
-      header.classList.add('menu-bar')
-      setTimeout(()=> header.style.top = '0', 100)
+function menuFixo() {
+  const header = document.querySelector(".headerFixoMenu");
+  const banner = document.querySelector(".banner-home");
+
+  if (header) {
+    if (window.pageYOffset > header.clientHeight && window.pageYOffset < 450) {
+      header.classList.add("menu-bar");
+      setTimeout(() => (header.style.top = "0"), 100);
+      isMenuActive = true;
+    } else if (
+      window.pageYOffset >= header.clientHeight &&
+      header.classList.contains("menu-bar") &&
+      offset <= window.pageYOffset
+    ) {
+      header.style.top = "";
+      setTimeout(() => header.classList.remove("menu-bar"), 600);
+      isMenuActive = false;
+    } else if (
+      offset > window.pageYOffset &&
+      !header.classList.contains("menu-bar")
+    ) {
+      header.classList.add("menu-bar");
+      setTimeout(() => (header.style.top = "0"), 100);
+      isMenuActive = true;
     }
-      offset = window.pageYOffset
-   
-    if(window.pageYOffset==0 ) {
-      header.classList.remove('menu-bar'); 
-      setTimeout(()=> header.style.top = '', 100)
+    offset = window.pageYOffset;
+
+    if (window.pageYOffset == 0) {
+      header.classList.remove("menu-bar");
+      setTimeout(() => (header.style.top = ""), 100);
+      isMenuActive = false;
     }
   }
-  
+
+  // Atualiza o margin-top da classe .banner-home de acordo com o estado de isMenuActive
+  if (isMenuActive) {
+    banner.style.marginTop = "0";
+  } else {
+    banner.style.marginTop = "-110px";
+  }
 }
-menuFixo()
 
+menuFixo();
 
-window.addEventListener('scroll', debounce(menuFixo,140))
+window.addEventListener("scroll", debounce(menuFixo, 140));
+
+// Função debounce para limitar a frequência de chamadas durante o scroll
+function debounce(func, delay) {
+  let timer;
+  return function () {
+    clearTimeout(timer);
+    timer = setTimeout(func, delay);
+  };
+}
